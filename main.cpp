cbc63951f5154b9492a4e071611893da9621cf2e
Essoh-Roddick <roddick388@gmail.com>
2024-10-31 13:54:43 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string str[2][4]={
+    {"A","B","C","D"},
+    {"E","F","G","H"}
+    };
+    cout<<str;
+/*string A[4]={"dick","ickd","ckdi","kdic"};
+string  B="I am a boy";;
+for(string i:A){
+    cout<< i<<endl;
+}
+ int b=sizeof(B)/sizeof(string);
+    cout <<"The size is :"<<b <<"\n";
+    cout<<sizeof(B);
+    while(A[i]!='\0')
+/*int num[10]={1,2,3,4,5,4,6,5,7,9};
+
+ for(int i:num){
+    cout<<i <<"\n";
+
+ }
+ int b=sizeof(num)/sizeof(int);
+  cout<<"The size of the array is:"<<b;
+ /*char A[4];
+ for(char i : A){
+    cin>>i;
+ }
+ for(char i : A){
+    cout<<i;
+ }
+ int b=sizeof(A);
+ cout<<b;*/
+ return 0;
+}
